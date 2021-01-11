6e272fd5d1273c9e383dcab8a22ab496316493a9
332018 <72552532+332018@users.noreply.github.com>
2021-01-11 23:04:39 +0100

    Povezan interfejs sa SchedulingBusiness

--- Classroom schedule/BusinessLogicLayer/SchedulingBusiness.cs
+++ Classroom schedule/BusinessLogicLayer/SchedulingBusiness.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Shared.Interfaces.Business;
+using Shared.Interfaces.Repository;
+using Shared.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,41 @@ using System.Threading.Tasks;
 
 namespace BusinessLogicLayer
 {
-    public class SchedulingBusiness
+    public class SchedulingBusiness : ISchedulingBusiness
     {
+        private readonly ISchedulingRepository schedulingRepository;
+        public SchedulingBusiness(ISchedulingRepository schedulingRepository)
+        {
+            this.schedulingRepository = schedulingRepository;
+        }
+        //*************************************************************************************************
+        public List<Scheduling> GetAllSchedulings()
+        {
+            return schedulingRepository.GetSchedulings();
+        }
+        //*************************************************************************************************
+        public Boolean InsertOneScheduling(Scheduling s)
+        {
+            if (this.schedulingRepository.InsertScheduling(s) > 0)
+                return true;
+            else
+                return false;
+        }
+        //*************************************************************************************************
+        public Boolean UpdateOneScheduling(Scheduling s)
+        {
+            if (schedulingRepository.UpdateScheduling(s) > 0)
+                return true;
+            else
+                return false;
+        }
+        //*************************************************************************************************
+        public Boolean DeleteOneScheduling(Scheduling s)
+        {
+            if (schedulingRepository.DeleteScheduling(s) > 0)
+                return true;
+            else
+                return false;
+        }
     }
 }
