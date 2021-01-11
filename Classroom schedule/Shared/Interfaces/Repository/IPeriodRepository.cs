fd078b927240f9a1ce340baa25b0e27b80ea923f
DESKTOP-NJHBCTJ\Filip <filip6331@gmail.com>
2021-01-11 13:33:28 +0100

    Ispravljene pravopisne greške.

--- Classroom schedule/Shared/Interfaces/Repository/IPeriodRepository.cs
+++ Classroom schedule/Shared/Interfaces/Repository/IPeriodRepository.cs
@@ -9,7 +9,7 @@ namespace Shared.Interfaces.Repository
 {
     public interface IPeriodRepository
     {
-        List<Period> GetPeriod();
+        List<Period> GetPeriods();
         int InsertPeriod(Period p);
         int UpdatePeriod(Period p);
     }
