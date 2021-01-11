76c1fcf5c836b92fdfa60b20279d1514e566b563
DESKTOP-NJHBCTJ\Filip <filip6331@gmail.com>
2021-01-11 13:44:06 +0100

    ZAVRŠEN SLOJ "Shared"!

---
+++ Classroom schedule/Shared/Interfaces/Business/ISchedulingBusiness.cs
@@ -0,0 +1,17 @@
+﻿using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Interfaces.Business
+{
+    public interface ISchedulingBusiness
+    {
+        List<Scheduling> GetAllSchedulings();
+        Boolean InsertOneScheduling(Scheduling s);
+        Boolean UpdateOneScheduling(Scheduling s);
+        Boolean DeleteOneScheduling(Scheduling s);
+    }
+}
