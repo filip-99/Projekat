ffce91ee48925a7a76aed222912a8b7d34ba34c8
332018 <72552532+332018@users.noreply.github.com>
2021-01-11 22:20:30 +0100

    Kreiranje klase za funkcionalnost forme

--- Classroom schedule/Classroom schedule/Login.cs
+++ Classroom schedule/Classroom schedule/Login.cs
@@ -12,9 +12,15 @@ namespace Classroom_schedule
 {
     public partial class Login : Form
     {
+       
         public Login()
         {
             InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
