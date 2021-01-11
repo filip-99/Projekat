f41cafbc8551262c10f1316297eb8400601cd287
332018 <72552532+332018@users.noreply.github.com>
2021-01-11 22:54:40 +0100

    Povezan interfejs sa PeriodClassroom

--- Classroom schedule/BusinessLogicLayer/PeriodBusiness.cs
+++ Classroom schedule/BusinessLogicLayer/PeriodBusiness.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Shared.Interfaces.Repository;
+using Shared.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,32 @@ namespace BusinessLogicLayer
 {
     public class PeriodBusiness
     {
+        private readonly IPeriodRepository periodRepository;
 
+        public PeriodBusiness(IPeriodRepository periodRepository)
+        {
+            this.periodRepository = periodRepository;
+        }
+
+        public List<Period> GetAllPeriods()
+        {
+            return periodRepository.GetPeriods();
+        }
+        //*************************************************************************************************
+        public Boolean InsertOnePeriod(Period p)
+        {
+            if (this.periodRepository.InsertPeriod(p) > 0)
+                return true;
+            else
+                return false;
+        }
+        //*************************************************************************************************
+        public Boolean UpdateOnePeriod(Period p)
+        {
+            if (periodRepository.UpdatePeriod(p) > 0)
+                return true;
+            else
+                return false;
+        }
     }
 }
