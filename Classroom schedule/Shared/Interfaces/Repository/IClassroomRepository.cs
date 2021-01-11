984f20e0d6815791b4d6a13ccd0157232a495d80
DESKTOP-NJHBCTJ\Filip <filip6331@gmail.com>
2021-01-11 13:28:54 +0100

    Kreirani interfejsi sa definicijama za metoda koje bi trablo da poseduje "DataAccessLayer".

---
+++ Classroom schedule/Shared/Interfaces/Repository/IClassroomRepository.cs
@@ -0,0 +1,16 @@
+﻿using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Interfaces.Repository
+{
+    public interface IClassroomRepository
+    {
+        List<Classroom> GetClassroom();
+        int InsertClassroom(Classroom c);
+        int UpdateClassroom(Classroom c);
+    }
+}
---
+++ Classroom schedule/Shared/Interfaces/Repository/ISchedulingRepository.cs
@@ -0,0 +1,17 @@
+﻿using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Interfaces.Repository
+{
+    public interface ISchedulingRepository
+    {
+        List<Scheduling> GetRezervacije();
+        int InsertScheduling(Scheduling s);
+        int UpdateScheduling(Scheduling s);
+        int DeleteScheduling(Scheduling s);
+    }
+}
