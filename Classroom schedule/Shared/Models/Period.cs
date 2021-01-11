c78df716af608355bdbd4df9a0360492267a5ebd
DESKTOP-NJHBCTJ\Filip <filip6331@gmail.com>
2021-01-11 13:15:14 +0100

    Kreiran "Shared" sloj. Dodate su klase (Classroom, Period, Scheduling) u folderu models.

---
+++ Classroom schedule/Shared/Models/Period.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class Period
+    {
+        private int id;
+        private TimeSpan start_time;
+        private TimeSpan end_time;
+
+        public int Id { get => id; set => id = value; }
+        public TimeSpan Start_time { get => start_time; set => start_time = value; }
+        public TimeSpan End_time { get => end_time; set => end_time = value; }
+    }
+}
